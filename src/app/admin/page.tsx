965b25e0699333875937ff8716da94a5a7eb2216
nanahiryu <ryu2751x7@gmail.com>
2023-09-02 23:27:59 +0900

    adminページ追加

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,14 @@
+import styles from "./page.module.scss";
+
+const Admin = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main_screen_field} />
+      <div className={styles.scrollable_field} />
+      <div className={styles.scrollable_field} />
+      <div />
+    </div>
+  );
+};
+
+export default Admin;
