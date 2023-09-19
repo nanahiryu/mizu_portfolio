32b7efcd4c98cc433c10f370e1241b0828ba5211
nanahiryu <ryu2751x7@gmail.com>
2023-09-20 01:07:54 +0900

    headerにhome追加

--- src/app/portfolio/_components/header.tsx
+++ src/app/portfolio/_components/header.tsx
@@ -7,6 +7,9 @@ const Header = () => {
   return (
     <div className={styles.menu}>
       <div className={styles.menu_item}>
+        <Link href="/portfolio">home</Link>
+      </div>
+      <div className={styles.menu_item}>
         <Link href="/portfolio/about">about</Link>
       </div>
       <div className={styles.menu_item}>
